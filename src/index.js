786a2a8106842951d337fb880ff70b3085f67803
betty-byte <bettycup8@gmail.com>
2024-10-23 12:12:11 +0200

    Added more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,41 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#weather-temperature");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#weather-app-city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let discriptionElement = document.querySelector("#scatered");
 
   city.innerHTML = response.data.city;
+  timeElement.innerHTML = formatedDate(date);
+  discriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatedDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
