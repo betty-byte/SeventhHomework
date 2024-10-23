a7d9f2dd56a3359781b99bfb662c6b0c8bb8427c
betty-byte <bettycup8@gmail.com>
2024-10-23 13:21:21 +0200

    Added more details on the icon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function updateWeather(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let discriptionElement = document.querySelector("#scatered");
+  let iconElement = document.querySelector(".icon");
 
   city.innerHTML = response.data.city;
   timeElement.innerHTML = formatedDate(date);
@@ -14,6 +15,7 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-icon"/>`;
 }
 
 function formatedDate(date) {
