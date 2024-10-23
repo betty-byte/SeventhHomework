a925b055d68bf47cb1a83f5182435720a65a6edc
betty-byte <bettycup8@gmail.com>
2024-10-23 05:31:46 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#weather-app-city");
+
+  city.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c2bo39f2at924270a681947bc398017a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#weather-app-city");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Welkom");
