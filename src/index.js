06e8514df8f505d35c9cac787717c899cd3b69ad
betty-byte <bettycup8@gmail.com>
2024-10-23 04:41:06 +0200

    Search Engine immigration redone

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function subSearch(event) {
+function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  let city = document.querySelector("#weather-app-city");
+  city.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", subSearch);
+searchFormElement.addEventListener("submit", handleSearch);
